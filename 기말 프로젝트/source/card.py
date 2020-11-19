6405b6fdd463f30dc14c88987b6eaf00ea70bb89
빅토리아 2세 주디윤 <61190721+sungzzuu@users.noreply.github.com>
2020-11-20 03:32:50 +0900

    햇살 개수에 따른 카드 색 변화
    각각의 스코어만큼 쌓이면
    카드가 밝아진다
    
    햇살이 적어지면 또
    어두워지는 것도 추가할 것

--- 기말 프로젝트/source/card.py
+++ 기말 프로젝트/source/card.py
@@ -1,25 +1,38 @@
 from pico2d import *
 import gfw
+import sun
 
 START_POS = 125, 40
 INTERVAL = 4
 
+STATE_ON = 1
+STATE_OFF = 0
+
 class Card:
     def __init__(self, order, name):
+        self.state = STATE_OFF
         if name == 'peashooter':
-            image_name = '../res/interface/card/PeashooterSeedPacket0.png'
+            self.image_name = '../res/interface/card/PeashooterSeedPacket'
+            self.score = 100
         elif name == 'sunflower':
-            image_name = '../res/interface/card/SunflowerSeedPacket0.png'
+            self.image_name = '../res/interface/card/SunflowerSeedPacket'
+            self.score = 50
         elif name == 'snowshooter':
-            image_name = '../res/interface/card/SnowPeaSeedPacket0.png'
+            self.image_name = '../res/interface/card/SnowPeaSeedPacket'
+            self.score = 175
         elif name == 'cherrybomb':
-            image_name = '../res/interface/card/CherryBombSeedPacket0.png'
+            self.image_name = '../res/interface/card/CherryBombSeedPacket'
+            self.score = 150
         elif name == 'wallnut':
-            image_name = '../res/interface/card/Wall-nutSeedPacket0.png'
-        self.image = gfw.image.load(image_name)
+            self.image_name = '../res/interface/card/Wall-nutSeedPacket'
+            self.score = 50
+        self.image = gfw.image.load(self.image_name + '0' + '.png')
         self.pos = START_POS[0] + order * (INTERVAL + self.image.w), get_canvas_height() - START_POS[1]
 
     def update(self):
-        pass
+        if sun.sun_score >= self.score:
+            self.image = gfw.image.load(self.image_name + '1' + '.png')
+            self.state = STATE_OFF
+
     def draw(self):
         self.image.draw(*self.pos)
